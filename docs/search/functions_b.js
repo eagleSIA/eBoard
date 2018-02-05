d23e37814770d16674f07756c9009e0b5ace9a64
eagleSIA <36164885+eagleSIA@users.noreply.github.com>
2018-02-05 19:09:25 +0100

    Add files via upload

--- docs/search/functions_b.js
+++ docs/search/functions_b.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['read',['read',['../structI2CInOut.html#a5446f02a6b5cb6a2e47dc3af03d4a692',1,'I2CInOut']]],
+  ['readi2c',['readI2C',['../eBoard_8h.html#a8f9fa5f96b3e529e43d56b569cb4e957',1,'eBoard.h']]],
   ['readpin',['readPin',['../eBoard_8h.html#aca4f96af7c6736bf2b61c88d731af741',1,'eBoard.h']]],
   ['reset',['reset',['../structSoccerBoard.html#a4318ef2a49b14d4e0d2dff6af662fd9c',1,'SoccerBoard::reset()'],['../structServoCds55.html#a0938b600a10dc996457162e20ec5388b',1,'ServoCds55::Reset()']]],
   ['rotate',['rotate',['../structServoCds55.html#ac8ba8ae3a1eb1f8cd479b83942eef674',1,'ServoCds55']]]

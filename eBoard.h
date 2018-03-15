466ad092f87ace35da88afd6ff26e354204ebc6a
eagleSIA <florian.sihler@web.de>
2018-03-15 16:51:43 +0100

    update

--- eBoard.h
+++ eBoard.h
@@ -375,6 +375,7 @@
 
       - Documentation for all implemented features
       - This code is now a vaild ArduinoIDE library
+        \n &nbsp; => <b> including Syntax highlighting </b>
 
       <b>Changed</b>
 
@@ -502,17 +503,6 @@
     #define TWI_SRX   3
     #define TWI_STX   4
 
-    void twi_init(void);
-    void twi_setAddress(uint8_t);
-    uint8_t twi_readFrom(uint8_t, uint8_t*, uint8_t, uint8_t);
-    uint8_t twi_writeTo(uint8_t, uint8_t*, uint8_t, uint8_t, uint8_t);
-    uint8_t twi_transmit(const uint8_t*, uint8_t);
-    void twi_attachSlaveRxEvent( void (*)(uint8_t*, int) );
-    void twi_attachSlaveTxEvent( void (*)(void) );
-    void twi_reply(uint8_t);
-    void twi_stop(void);
-    void twi_releaseBus(void);
-
     #include <math.h>
     #include <stdlib.h>
     #include <inttypes.h>
@@ -566,20 +556,17 @@
         TWCR = _BV(TWEN) | _BV(TWIE) | _BV(TWEA);
       }
 
-      void twi_setAddress(uint8_t address) {
+      inline void twi_setAddress(uint8_t address) {
         TWAR = address << 1;
       }
 
       uint8_t twi_readFrom(uint8_t address, uint8_t* data, uint8_t length, uint8_t sendStop) {
         uint8_t i;
 
-        if(TWI_BUFFER_LENGTH < length){
-          return 0;
-        }
+        if(TWI_BUFFER_LENGTH < length) return 0;
 
-        while(TWI_READY != twi_state){
-          continue;
-        }
+        while(TWI_READY != twi_state) continue;
+
         twi_state = TWI_MRX;
         twi_sendStop = sendStop;
 
@@ -598,31 +585,23 @@
         else
           TWCR = _BV(TWEN) | _BV(TWIE) | _BV(TWEA) | _BV(TWINT) | _BV(TWSTA);
 
-        while(TWI_MRX == twi_state){
-          continue;
-        }
+        while(TWI_MRX == twi_state) continue;
 
         if (twi_masterBufferIndex < length)
           length = twi_masterBufferIndex;
 
-        for(i = 0; i < length; ++i){
-          data[i] = twi_masterBuffer[i];
-        }
+        for(i = 0; i < length; ++i) data[i] = twi_masterBuffer[i];
 
         return length;
       }
 
-      uint8_t twi_writeTo(uint8_t address, uint8_t* data, uint8_t length, uint8_t wait, uint8_t sendStop)
-      {
+      uint8_t twi_writeTo(uint8_t address, uint8_t* data, uint8_t length, uint8_t wait, uint8_t sendStop) {
         uint8_t i;
 
-        if(TWI_BUFFER_LENGTH < length){
-          return 1;
-        }
+        if(TWI_BUFFER_LENGTH < length) return 1;
 
-        while(TWI_READY != twi_state){
-          continue;
-        }
+        while(TWI_READY != twi_state) continue;
+
         twi_state = TWI_MTX;
         twi_sendStop = sendStop;
         twi_error = 0xFF;
@@ -630,9 +609,7 @@
         twi_masterBufferIndex = 0;
         twi_masterBufferLength = length;
 
-        for(i = 0; i < length; ++i){
-          twi_masterBuffer[i] = data[i];
-        }
+        for(i = 0; i < length; ++i) twi_masterBuffer[i] = data[i];
 
         twi_slarw = TW_WRITE;
         twi_slarw |= address << 1;
@@ -645,35 +622,23 @@
         else
           TWCR = _BV(TWINT) | _BV(TWEA) | _BV(TWEN) | _BV(TWIE) | _BV(TWSTA);	// enable INTs
 
-        while(wait && (TWI_MTX == twi_state)){
-          continue;
-        }
+        while(wait && (TWI_MTX == twi_state)) continue;
 
-        if (twi_error == 0xFF)
-          return 0;
-        else if (twi_error == TW_MT_SLA_NACK)
-          return 2;
-        else if (twi_error == TW_MT_DATA_NACK)
-          return 3;
-        else
-          return 4;
+        if (twi_error == 0xFF) return 0;
+        else if (twi_error == TW_MT_SLA_NACK) return 2;
+        else if (twi_error == TW_MT_DATA_NACK) return 3;
+        else return 4;
       }
 
       uint8_t twi_transmit(const uint8_t* data, uint8_t length) {
         uint8_t i;
 
-        if(TWI_BUFFER_LENGTH < length){
-          return 1;
-        }
+        if(TWI_BUFFER_LENGTH < length) return 1;
 
-        if(TWI_STX != twi_state){
-          return 2;
-        }
+        if(TWI_STX != twi_state) return 2;
 
         twi_txBufferLength = length;
-        for(i = 0; i < length; ++i){
-          twi_txBuffer[i] = data[i];
-        }
+        for(i = 0; i < length; ++i) twi_txBuffer[i] = data[i];
 
         return 0;
       }
@@ -698,9 +663,7 @@
       void twi_stop(void) {
         TWCR = _BV(TWEN) | _BV(TWIE) | _BV(TWEA) | _BV(TWINT) | _BV(TWSTO);
 
-        while(TWCR & _BV(TWSTO)){
-          continue;
-        }
+        while(TWCR & _BV(TWSTO)) continue;
 
         twi_state = TWI_READY;
       }
@@ -724,13 +687,12 @@
               TWDR = twi_masterBuffer[twi_masterBufferIndex++];
               twi_reply(1);
             }else{
-      	if (twi_sendStop)
-                twi_stop();
-      	else {
-      	  twi_inRepStart = true;
-      	  TWCR = _BV(TWINT) | _BV(TWSTA)| _BV(TWEN) ;
-      	  twi_state = TWI_READY;
-      	}
+            	if (twi_sendStop) twi_stop();
+            	else {
+            	  twi_inRepStart = true;
+            	  TWCR = _BV(TWINT) | _BV(TWSTA)| _BV(TWEN) ;
+            	  twi_state = TWI_READY;
+            	}
             }
             break;
           case TW_MT_SLA_NACK:
@@ -745,26 +707,21 @@
             twi_error = TW_MT_ARB_LOST;
             twi_releaseBus();
             break;
-
           case TW_MR_DATA_ACK:
             twi_masterBuffer[twi_masterBufferIndex++] = TWDR;
           case TW_MR_SLA_ACK:
-            if(twi_masterBufferIndex < twi_masterBufferLength){
-              twi_reply(1);
-            }else{
-              twi_reply(0);
-            }
+            if(twi_masterBufferIndex < twi_masterBufferLength) twi_reply(1);
+            else twi_reply(0);
             break;
           case TW_MR_DATA_NACK:
             twi_masterBuffer[twi_masterBufferIndex++] = TWDR;
-      	if (twi_sendStop)
-                twi_stop();
-      	else {
-      	  twi_inRepStart = true;
-      	  TWCR = _BV(TWINT) | _BV(TWSTA)| _BV(TWEN) ;
-      	  twi_state = TWI_READY;
-      	}
-      	break;
+          	if (twi_sendStop) twi_stop();
+          	else {
+          	  twi_inRepStart = true;
+          	  TWCR = _BV(TWINT) | _BV(TWSTA)| _BV(TWEN) ;
+          	  twi_state = TWI_READY;
+          	}
+        	break;
           case TW_MR_SLA_NACK:
             twi_stop();
             break;
@@ -781,14 +738,11 @@
             if(twi_rxBufferIndex < TWI_BUFFER_LENGTH){
               twi_rxBuffer[twi_rxBufferIndex++] = TWDR;
               twi_reply(1);
-            }else{
-              twi_reply(0);
-            }
+            } else twi_reply(0);
+
             break;
           case TW_SR_STOP:
-            if(twi_rxBufferIndex < TWI_BUFFER_LENGTH){
-              twi_rxBuffer[twi_rxBufferIndex] = '\0';
-            }
+            if(twi_rxBufferIndex < TWI_BUFFER_LENGTH) twi_rxBuffer[twi_rxBufferIndex] = '\0';
             twi_stop();
             twi_onSlaveReceive(twi_rxBuffer, twi_rxBufferIndex);
             twi_rxBufferIndex = 0;
@@ -810,11 +764,8 @@
             }
           case TW_ST_DATA_ACK:
             TWDR = twi_txBuffer[twi_txBufferIndex++];
-            if(twi_txBufferIndex < twi_txBufferLength){
-              twi_reply(1);
-            }else{
-              twi_reply(0);
-            }
+            if(twi_txBufferIndex < twi_txBufferLength) twi_reply(1);
+            else twi_reply(0);
             break;
           case TW_ST_DATA_NACK:
           case TW_ST_LAST_DATA:

be21b9b5a93619a49a8ebfe3721ea969bab67257
eagleSIA <florian.sihler@web.de>
2018-04-21 10:38:31 +0200

    Update 3.1.19m

---
+++ docs/eagle__PreprocessorControl_8h.js
@@ -0,0 +1,4 @@
+var eagle__PreprocessorControl_8h =
+[
+    [ "PREPROCESS_DEBUG", "eagle__PreprocessorControl_8h.html#a2423814a8c1071bb3f82017115574158", null ]
+];
---
+++ source/eagle_PreprocessorControl.h
@@ -0,0 +1,22 @@
+//This was created by EagleoutIce 'document creator: create_doc' using doxygen 1.8.15 and python 3.5.2
+//Created: 20.04.2018 22:20:28
+
+// This file contains various preprocessor tools provided by eBoard
+#define VALUE_TO_STRING(x) #x
+#ifdef DOC
+	 
+	 #define PREPROCESS_DEBUG 1
+#endif
+#define VALUE(x) VALUE_TO_STRING(x)
+#ifndef PREPROCESS_DEBUG
+ #define PREPROCESS_DEBUG 0
+#endif
+#define PPERFORM_PRAGMA(str) _Pragma(#str)
+#if PREPROCESS_DEBUG > 0
+ #pragma message("You are using eBoard-header 🐉" VALUE(EBOARD_VERSION) "{" VALUE(EBOARD_VERSION_NBR) "} written by EagleoutIce")
+ #define DEBUG_MSG(str) PPERFORM_PRAGMA(message ("" #str))
+ #define MACRO_MSG(mac,str) PPERFORM_PRAGMA(message("You set " #mac " to " VALUE(mac) ": " #str))
+#else
+ #define DEBUG_MSG(str) ;
+ #define MACRO_MSG(mac,str) ;
+#endif

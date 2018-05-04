a2f6eb56160860f8a87327c6cd2bcbf72ad3f49c
eagleSIA <florian.sihler@web.de>
2018-05-04 22:51:45 +0200

    update 3.2.26d

--- source/eagle_Twi.h
+++ source/eagle_Twi.h
@@ -1,5 +1,5 @@
 //This was created by EagleoutIce 'document creator: create_doc' using doxygen 1.8.15 and python 3.5.2
-//Created: 04.05.2018 22:46:51
+//Created: 04.05.2018 22:51:25
 #ifndef EAGLE_EBOARD_HELPLIB_TWI
     #define EAGLE_EBOARD_HELPLIB_TWI
 
